edaa21d54c473cfbcd8ce8aadb173009c3ba5e45
Tomeski05@github.com <tomeski@protonmail.com>
2022-03-11 15:17:09 +0100

    SQL Tasks

--- SQLQuery8.sql
+++ SQLQuery8.sql
@@ -422,5 +422,42 @@ Where EmployeeID In (
 	)
 
 
--- 8.8 The hospital has several examination rooms where appointments take place. Obtain the number of appointments that have taken place in each examination room.
-Select 
+ --8.8 The hospital has several examination rooms where appointments take place. Obtain the number of appointments that have taken place in each examination room.
+--Select ExaminationRoom, Count(AppointmentID) As "Number"
+--From Appointment
+--Group by ExaminationRoom
+
+
+Select Cast(ExaminationRoom As nvarchar(100)) Room, Count(AppointmentID) As "Number"
+From Appointment
+Group by Cast(ExaminationRoom As nvarchar(100))
+
+
+
+-- 8.9 Obtain the names of all patients (also include, for each patient, the name of the patient's primary care physician), such that \emph{all} the following are true:
+    -- The patient has been prescribed some medication by his/her primary care physician.
+    -- The patient has undergone a procedure with a cost largqer that $5,000
+    -- The patient has had at least two appointment where the nurse who prepped the appointment was a registered nurse.
+    -- The patient's primary care physician is not the head of any department.
+
+
+Select Pt.Name As PatientName, PhPCP.Name As PhsycianName
+From Patient Pt, Physician PhPCP
+Where Pt.PCP = PhPCP.EmployeeID
+And Exists (
+	Select *
+	From Prescribes Pr
+	Where Pr.Patient = Pt.SSN
+	And Pr.Physician = Pt.PCP
+	)
+And Exists (
+	Select *
+	From Undergoes U, Procedures Pr
+	Where U.Procedures = Pr.Code
+	And U.Patient = Pt.SSN
+	And Pr.Cost > 5000
+	)
+And 2<= (
+	Select Count(A.AppointmentID)
+	From Appointment a, Nurse N
+
